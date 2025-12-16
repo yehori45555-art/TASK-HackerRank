17d85e4d81c08bc13797a1d0f581775afb6ded8b
yehori45555-art <yehori4.5555@gmail.com>
2025-12-16 19:35:21 +0200

    Commit

---
+++ task3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+	int main(){
+        // 3. Subtract two numbers. 
+        int a,b;
+        std::cout << "Instert a:" << std::endl;
+        std::cin >> a;
+        std::cout << "Instert b:" << std::endl;
+        std::cin >> b;
+
+        std::cout << "Result:" << a - b << std::endl;
+}
