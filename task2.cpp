ca8070a1e74459ec83e071400262205f2861f298
yehori45555-art <yehori4.5555@gmail.com>
2025-12-16 19:33:48 +0200

    Commit

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+	int main()
+{
+        // 2.Add two numbers. 
+        int a,b;
+        std::cout << "Instert a:" << std::endl;
+        std::cin >> a;
+        std::cout << "Instert b:" << std::endl;
+        std::cin >> b;
+
+        std::cout << "Result:" << a + b << std::endl;
+}
